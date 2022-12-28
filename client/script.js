906eba373a7ca9be392fbc99521e69b789d4ef5d
Oleksandr Gneushev <galexing@gmail.com>
2022-12-28 23:49:05 +0100

    add live server path

--- client/script.js
+++ client/script.js
@@ -68,7 +68,7 @@ async function handleSubmit(e) {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-openai-chat.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
